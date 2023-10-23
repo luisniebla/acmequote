70c46cc6b8a6e2dc26ad912ad91166327e8dc092
Luis Niebla <lrios@repay.com>
2023-10-22 21:01:06 -0700

    Initial model work

--- api/quote/admin.py
+++ api/quote/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Coverage, Quote, State
+
 # Register your models here.
+admin.site.register(Quote)
+admin.site.register(State)
+admin.site.register(Coverage)
--- api/quote/models.py
+++ api/quote/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Coverage(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=4, max_digits=5)
+    state_rates = models.ForeignKey('State', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    name = models.CharField(max_length=200)
+    tax = models.DecimalField(decimal_places=4, max_digits=5)
+
+    def __str__(self):
+        return self.name
+
+
+class Quote(models.Model):
+    class CovereageType(models.TextChoices):
+        BASIC = "BS", _("Basic")
+        PREMIUM = "PR", _("Premium")
+
+    name = models.CharField(max_length=200)
+    coverage_type = models.CharField(max_length=2, choices=CovereageType.choices)
+    state = models.ForeignKey(
+        'State',
+        on_delete=models.CASCADE,
+    )
+    coverages = models.ManyToManyField(Coverage)
+
+    def __str__(self):
+        return self.name
