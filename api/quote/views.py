cfc708cb538e1b0c223b5c4b28108dbed18377c2
Luis Niebla <lrios@repay.com>
2023-10-22 23:28:21 -0700

    generate_price, fix test data

--- api/quote/views.py
+++ api/quote/views.py
@@ -1,5 +1,53 @@
+from decimal import Decimal
+
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from .models import CoverageSurcharge, Quote
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def generate_price(quote):
+    subtotal = 0
+    for coverage in quote.coverages.all():
+        subtotal += Decimal(coverage.monthly_price)
+
+    surcharges = CoverageSurcharge.objects.filter(
+        coverage__in=quote.coverages.all(),
+        state=quote.state,
+    )
+
+    for surcharge in surcharges:
+        subtotal = subtotal + subtotal * Decimal(surcharge.fee)
+
+    tax = Decimal(quote.state.tax) * Decimal(subtotal)
+    quant = lambda x: Decimal(x).quantize(Decimal('0.01'))
+    return {
+        'subtotal': quant(subtotal),
+        'taxes': quant(tax),
+        'total': quant(subtotal + tax),
+    }
+
+
+def index(request, resource_id):
+    quote = get_object_or_404(Quote, pk=resource_id)
+    print(quote.state)
+    print('Some other ti')
+    # TODO: Should add non-nullable primary_coverage to Quote
+    primary_coverage = (
+        quote.coverages.filter(name='Basic').first()
+        or quote.coverages.filter(name='Premium').first()
+    )
+    prices = generate_price(quote)
+    return HttpResponse(
+        f'''
+        {quote.name}:
+            Coverage Type: {primary_coverage.name}
+            State: {quote.state}
+            Has Pet: {quote.coverages.filter(name='Pet').exists()}
+            Flood Coverage: {quote.coverages.filter(name='Flood').exists()}
+            Price:
+                Monthly Subtotal: ${prices['subtotal']}
+                Monthly Taxes: ${prices['taxes']}
+                Monthly Total: ${prices['total']}
+        '''
+    )
