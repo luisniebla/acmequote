e4f72a99f902154c9382ae54c6aa3a3c16603bb4
Luis Niebla <lrios@repay.com>
2023-10-22 21:50:06 -0700

    Move surcharges to lookup table

--- api/quote/admin.py
+++ api/quote/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Coverage, Quote, State
+from .models import Coverage, CoverageSurcharge, Quote, State
 
 # Register your models here.
 admin.site.register(Quote)
 admin.site.register(State)
 admin.site.register(Coverage)
+admin.site.register(CoverageSurcharge)
--- api/quote/models.py
+++ api/quote/models.py
@@ -4,11 +4,28 @@ from django.utils.translation import gettext_lazy as _
 
 class Coverage(models.Model):
     name = models.CharField(max_length=200)
-    price = models.DecimalField(decimal_places=4, max_digits=5)
-    state_rates = models.ForeignKey('State', on_delete=models.CASCADE)
+    monthly_price = models.DecimalField(decimal_places=2, max_digits=10)
+    surcharge_fee = models.DecimalField(
+        decimal_places=4, max_digits=5, null=True, blank=True
+    )
+    area = models.ForeignKey(
+        'State',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
+
+
+class CoverageSurcharge(models.Model):
+    coverage = models.ForeignKey('Coverage', on_delete=models.CASCADE)
+    state = models.ForeignKey('State', on_delete=models.CASCADE)
+    fee = models.DecimalField(decimal_places=4, max_digits=5, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.coverage.name} - {self.state.name}'
 
 
 class State(models.Model):
